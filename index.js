300ebcde5e31313dd18d4822a50cc85e60044bc9
Carell Hamil <ayannahamil@gmail.com>
2018-11-21 17:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,6 @@ function theBeatlesPlay(musicians, instruments){
 //and adds !!! to end the of each statement
 const newFacts = [];
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
 var i = 0;
 
 function johnLennonFacts(facts){
